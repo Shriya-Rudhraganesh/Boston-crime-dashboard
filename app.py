136d339c3eb5da8b0bc1ea9409f2141f019509c3
Raquel <raquelanabush@gmail.com>
2025-11-16 22:23:38 -0500

    update title

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import altair as alt
 
 # streamlit setup:
 st.set_page_config(page_title="Boston Crime Insights", layout="wide")
-st.title("Boston Crime Dashboard")
+st.title("Boston Crime Insights")
 st.subheader("(Explore official BPD data crime incident reports from 2015 to present)")
 
 # Boston dataset API (updated daily):
